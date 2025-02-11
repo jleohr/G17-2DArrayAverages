45b6fd1ee08e3f94dcdeca2a4d6ad6114e95437c
Jonathan Leohr <40473237+jleohr@users.noreply.github.com>
2025-02-11 08:24:51 -0600

    Create Gee17.java

---
+++ Gee17.java
@@ -0,0 +1,9 @@
+public class Gee17{
+  public static void main(String[] args){
+    TwoDee twoDee = new TwoDee();
+
+    twoDee.rowAverages();
+    twoDee.columnAverages();
+    twoDee.arrayAverage();
+  }
+}
