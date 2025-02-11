20fd5f70454fa673379a258bf214546941f4a4b8
Jonathan Leohr <40473237+jleohr@users.noreply.github.com>
2025-02-11 08:22:40 -0600

    Create ArrayAverages.java

---
+++ ArrayAverages.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ArrayAverages{
+  
+  int[][] ray;
+
+  public ArrayAverages(int[][] rayray){
+    ray = rayray;
+  }
+
+  public void rowAverages(){
+    
+  }
+
+  public void columnAverages(){
+
+  }
+
+  public void arrayAverage(){
+
+  }
+
+  public String toString(){
+    for(int row=0; row<ray.length; row++)
+    {
+      System.out.println(Arrays.toString(ray[row]));
+    }
+  }
+}
