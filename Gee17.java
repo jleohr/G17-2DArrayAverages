f4af705117382a1ce422d79bdce4a9ecec439bc7
Jonathan Leohr <40473237+jleohr@users.noreply.github.com>
2025-02-11 08:50:47 -0600

    Update Gee17.java

--- Gee17.java
+++ Gee17.java
@@ -1,7 +1,19 @@
 public class Gee17{
   public static void main(String[] args){
-    ArrayAverages twoDee = new ArrayAverages();
 
+    int[][] ray = new int[7][7];
+
+    //Populate the array with sequential numbers
+    /*
+      1  2  3  4  5  6  7
+      8  9  10 11 12 13 14
+      15 16 17 18 19 20 21
+      22 23 24 25...
+      */
+
+    ArrayAverages twoDee = new ArrayAverages(ray);
+
+    System.out.println(twoDee);
     twoDee.rowAverages();
     twoDee.columnAverages();
     twoDee.arrayAverage();
